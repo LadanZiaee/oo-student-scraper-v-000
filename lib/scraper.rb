123c9373d12847cf37bd767a4d61daa6b34e6514
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-12 15:35:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,11 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     info = []
+<<<<<<< HEAD
     page = Nokogiri::HTML(open(index_url))
+=======
+    page = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
+>>>>>>> 31ffe1a317c0fb041dd4a9560ca2639cecebb7ac
     student = page.css(".student-card")
     student.each do |card|
       info << {:name => card.css("h4").text, :location => card.css("p").text, :profile_url => card.css("a")[0]["href"]}
@@ -16,6 +20,7 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     page = {}
+<<<<<<< HEAD
     profile = Nokogiri::HTML(open(profile_url))
     social = profile.css(".social-icon-container a")
     social.each do |link|
@@ -33,6 +38,10 @@ class Scraper
     page[:profile_quote] = profile.css(".profile-quote").text if profile.css(".profile-quote")
     page[:bio] = profile.css(".description-holder p").text
     page
+=======
+    profile = Nokogiri::HTML(open(info[:profile_url][0])
+    social = profile.css(".social-icon-container")
+>>>>>>> 31ffe1a317c0fb041dd4a9560ca2639cecebb7ac
   end
 
 end
